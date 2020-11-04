beee72bb5586ca3f5899c4f53d2cc772cd4e0592
bmallory <bmallory@bu.edu>
2020-11-04 17:44:37 -0500

    doodles

---
+++ noodling.R
@@ -0,0 +1,24 @@
+states_tm<-c("WV","FL")
+> a <- filter(states2, STUSPS==states_tm)
+> View(states2)
+> states_tm <- c("TX", "OK")
+> a <- filter(states2, STUSPS==states_tm)
+> states_tm <- c("TX", "OK")
+> a <- filter(states2, STUSPS==states_tm)
+a <- filter(states2, STUSPS==c("TX", "OK"))
+a <- filter(states2, STUSPS==c("TX", "OK"))
+a <- filter(states2, STUSPS %in% states_tm)
+
+
+tm_shape(states2) +
+  tm_borders(col="black", lwd=0.5) +
+tm_shape(floyd_rain2) +
+  tm_polygons(col="grey",lwd=0.1, "rain", id="fips") +
+  tm_style_beaver(title = "Floyd-1999", title.position = c("center", "top"))
+
+
+
+tm_shape(county2) +
+  tm_borders(col="grey", lwd=0.5)
+
+
